909e1531b2a3141f5848ddd14491b4707e05c071
edsonsousa33 <edson.sousa@compasso.com.br>
2021-10-13 11:33:59 -0300

    Agent_API: add route to get intents

--- src/server.js
+++ src/server.js
@@ -3,11 +3,17 @@ const dialogflow = require("dialogflow");
 const uuid = require("uuid");
 const dotenv = require('dotenv');
 
+// -----Sessions Client -----
 const app = customExpress();
 const projectId = 'curso-sdk-aibp';
 const sessionId = uuid.v4();
 const sessionClient = new dialogflow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(projectId, sessionId)
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// -----Intents Client -----
+const intentsClient = new dialogflow.IntentsClient();
+const projectAgentPath = intentsClient.projectAgentPath(projectId);
+
 
 const result = dotenv.config()
 
@@ -43,6 +49,21 @@ app.post('/detectIntent', (req, res) => {
   }).catch((err) => console.log(err))
   
 })
+
+
+app.get('/getIntents',async (req, res) => {
+  let [results] = await getIntents()
+
+  res.json(results)
+});
+
+async function getIntents(){
+  let params = {
+    intentView: 'INTENT_VIEW_FULL',
+    parent: projectAgentPath
+  }
+  return await intentsClient.listIntents(params);
+}
 
 app.listen(3000, () => {
   console.log('server is running')
